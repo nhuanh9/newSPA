6e36c3da6b09ec6d34d0e87a074ee4b0720bed21
nhuanh9 <chopperlouis@gmail.com>
2019-12-09 13:19:05 +0700

    add frontend for add house form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {RegisterComponent} from './Components/Pages/users/register/register.component';
 import {ListHouseComponent} from './Components/Pages/house/list-house/list-house.component';
 import {DetailHouseComponent} from './Components/Pages/house/detail-house/detail-house.component';
+import {CreateHouseComponent} from './Components/Pages/house/create-house/create-house.component';
 
 
 const routes: Routes = [
@@ -18,10 +19,15 @@ const routes: Routes = [
     path: 'detail-house/:id',
     component: DetailHouseComponent
   },
+  {
+    path: 'create-house',
+    component: CreateHouseComponent
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
