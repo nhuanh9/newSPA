7565e37eeb7b2bdf73901644d7598ccf741b428f
nhuanh9 <chopperlouis@gmail.com>
2019-12-10 16:30:02 +0700

    validate

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyCA06zFm_-bf9hHHgBY-uakCEr2InCoidQ',
+    authDomain: 'spa-stay.firebaseapp.com',
+    databaseURL: 'https://spa-stay.firebaseio.com',
+    projectId: 'spa-stay',
+    storageBucket: 'spa-stay.appspot.com',
+    messagingSenderId: '944237622450',
+    appId: '1:944237622450:web:3a5316ec1584e4589485ab',
+    measurementId: 'G-4DQHY2CY4G'
+  }
 };
