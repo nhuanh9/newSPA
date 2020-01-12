2eec3c1e284e52edebb0a7d4aa545843b4a17a47
Đào Như Anh <chopperlouis@gmail.com>
2020-01-12 23:10:59 +0700

    add searchService

---
+++ src/app/Components/Pages/house/search/services/search-service.service.ts
@@ -0,0 +1,34 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {House} from '../../../../../model/House';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchServiceService {
+  API_URL = 'https://demo-project-spa.herokuapp.com/host';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getList(): Observable<House[]> {
+    return this.http.get<House[]>(this.API_URL);
+  }
+
+  create(house): Observable<House> {
+    return this.http.post<House>(this.API_URL, house);
+  }
+
+  detail(id: string): Observable<House> {
+    return this.http.get<House>(this.API_URL + `/${id}`);
+  }
+
+  edit(house: House, id: string): Observable<House> {
+    return this.http.put<House>(this.API_URL + `/${id}`, house);
+  }
+
+  delete(id: string): Observable<House> {
+    return this.http.delete<House>(this.API_URL + `/${id}`);
+  }
+}
